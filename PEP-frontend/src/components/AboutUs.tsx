e90eac3bd942257ee21d68d80fc2e3dc2a4cdde4
Moussa Kalam AMZAT <m.amzat@alustudent.com>
2023-08-09 01:10:55 +0200

    feat: Add part of About Us page.

---
+++ PEP-frontend/src/components/AboutUs.tsx
@@ -0,0 +1,85 @@
+const AboutUs = () => {
+  return (
+    <>
+      <section>
+        <h1>About Us</h1>
+        <img src="" alt="" />
+        <h2>Our Story</h2>
+        <p>
+          MentorMe, the Passion Empowerment Platform, was born out of a single
+          idea: to break down the barriers that limit individual growth and
+          creativity. Our founder, Moussa, witnessed the untapped potential in
+          students back in his home country, and that ignited a fire with him to
+          create a space where passions could flourish
+        </p>
+
+        <h2>Our Vision</h2>
+        <img src="" alt="" />
+        <p>
+          Our vision is to revolutionize education by making it personal,
+          purposeful, and passionate. We envision a world where students aren't
+          just learners, but also explorers, creators, and leaders of their own
+          journey.
+        </p>
+
+        <h2>Meet the Founder</h2>
+        <img src="" alt="" />
+        <h3>Moussa Kalam AMZAT</h3>
+        <p>
+          A former diplomacy graduate, Virtual Assistant, self-taught English
+          speaker and Judo Instructor, turned tech enthusiast, Moussa's journey
+          inspired the platform's mission to bridge the gap between education
+          and empowerment. He believes in using technology to create a better
+          world and have a positive impact of the Educational system in his
+          country
+        </p>
+
+        <h2>Our Journey</h2>
+        <img src="" alt="" />
+        <p>
+          From the seed of an idea to a thriving community, our journey has been
+          filled with challenges, triumps, and the unwavering belief that
+          empowerment is possible for all.
+        </p>
+
+        <ul>
+          <li>
+            Inception: The idea for Passion Empowerment takes root, driven by
+            the need for alternative learning avenues.Inception: The idea for
+            this platform is a thriving{" "}
+          </li>
+          <li>
+            Development: The platform takes shape, with each line of code and
+            design element reflecting our commitment to user growth.
+          </li>
+          <li>
+            Launch:: MentorMe is introduced to the world, creating riplles of
+            excitement amoung students, mentors, and educators.
+          </li>
+          <li>
+            Impact: Success stories pour in, showcasing the impact of mentorship
+            and passion-driven learning on individual lives.
+          </li>
+        </ul>
+
+        <h2>Our impact</h2>
+        <p>
+          We measure our success by the lives we touch and the dreams we
+          inspire. With each mentorship connection, each creative endeavor, and
+          each newfound passion, we move closer to our vision of a world where
+          education knows no bounds.
+        </p>
+
+        <h2>Join our Movement</h2>
+        <p>
+          We invite you to be part of this revolution -- a movement that
+          empowers individuals to embrace their passions and embrace their
+          potential. Whether you're a student, a mentor, or simply curious,
+          there's a place for you in our community
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default AboutUs;
