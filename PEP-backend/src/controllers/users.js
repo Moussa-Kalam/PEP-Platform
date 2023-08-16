c75e721faddd215c86a47074a3845847e5ad16c9
Moussa Kalam AMZAT <m.amzat@alustudent.com>
2023-08-16 22:42:18 +0200

    fix: Install babel-cli correct deployment

--- PEP-backend/src/controllers/users.js
+++ PEP-backend/src/controllers/users.js
@@ -2,45 +2,53 @@ import { User } from "../db/models";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-
 export async function createUser(req, res) {
-    try {
-        const { fullName, dateOfBirth, username, gender = null, country, interests = null, email, password } = req.body;
-        const encryptedPassword = await bcrypt.hash(password, 10);
-        const userExists = await User.findOne({ where: { email } });
-        if (userExists) {
-          return res.status(409).json({
-            message: "Email already exists",
-          });
-        }
-        const user = await User.create({
-          fullName,
-          dateOfBirth,
-          username,
-          gender,
-          country,
-          interests,
-          email,
-          password: encryptedPassword,
-        });
-    
-        const newUser = {
-          id: user.id,
-          email: user.email,
-          username: user.username,
-        };
-        const token = jwt.sign(newUser, process.env.SECRET_KEY);
-        return res.status(201).json({
-          message: "User created",
-          user: newUser,
-          token,
-        });
-      } catch (error) {
-        return res.status(500).json({
-          message: "Something went wrong",
-          error: error.message,
-        });
-      }
+  try {
+    const {
+      fullName,
+      dateOfBirth,
+      username,
+      gender = null,
+      country,
+      interests = null,
+      email,
+      password,
+    } = req.body;
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const userExists = await User.findOne({ where: { email } });
+    if (userExists) {
+      return res.status(409).json({
+        message: "Email already exists",
+      });
+    }
+    const user = await User.create({
+      fullName,
+      dateOfBirth,
+      username,
+      gender,
+      country,
+      interests,
+      email,
+      password: encryptedPassword,
+    });
+
+    const newUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+    const token = jwt.sign(newUser, process.env.SECRET_KEY);
+    return res.status(201).json({
+      message: "User created",
+      user: newUser,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
 }
 
 export async function signin(req, res) {
@@ -62,8 +70,10 @@ export async function signin(req, res) {
       id: user.id,
       email: user.email,
       username: user.username,
+      fullName: user.fullName,
     };
     const token = jwt.sign(newUser, process.env.SECRET_KEY);
+
     return res.status(200).json({
       message: "User logged in",
       user: newUser,
